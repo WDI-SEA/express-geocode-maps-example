3af6a1f15779aa11898d321eb6c53a529ac54993
Connor Clark <cjamcl@gmail.com>
2017-04-22 20:52:24 -0700

    using rowdy-logger module

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
+var rowdy = require('rowdy-logger');
 var app = express();
+
+rowdy.begin(app);
 
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
@@ -11,24 +14,26 @@ app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  db.place.findAll().then(function(places) {
-    res.render('index', { places: places });
-  }).catch(function(err) {
-    res.send({ message: 'error', error: err });
-  });
+    db.place.findAll().then(function(places) {
+        res.render('index', { places: places });
+    }).catch(function(err) {
+        res.send({ message: 'error', error: err });
+    });
 });
 
 app.post('/places', function(req, res) {
-  db.place.create({
-    name: req.body.name,
-    address: req.body.address
-  }).then(function(place) {
-    res.redirect('/');
-  }).catch(function(err) {
-    res.send({ message: 'error', error: err });
-  });
+    db.place.create({
+        name: req.body.name,
+        address: req.body.address
+    }).then(function(place) {
+        res.redirect('/');
+    }).catch(function(err) {
+        res.send({ message: 'error', error: err });
+    });
 });
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, function() {
+    rowdy.print();
+});
 
 module.exports = server;
